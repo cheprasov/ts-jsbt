acd54c63bc74e7e51550e2c978ba40fadbd2da84
Alexander Cheprasov <acheprasov84@gmail.com>
2023-08-23 23:25:01 +0100

    wip

--- src/converter/doubleToBytes.ts
+++ src/converter/doubleToBytes.ts
@@ -1,10 +1,9 @@
-export doubleToBytes = (value: number): [number, number, number, number, number, number, number] => {
-    var buffer = new ArrayBuffer(8); // JS numbers are 8 bytes long, or 64 bits
-    var longNum = new Float64Array(buffer); // so equivalent to Float64
+export type TUint8 = [number, number, number, number, number, number, number]
 
+export const doubleToBytes = (value: number, bigEndianOrder = true ): TUint8 => {
+    const buffer = new ArrayBuffer(8); // JS numbers are 8 bytes long, or 64 bits
+    const longNum = new Float64Array(buffer); // so equivalent to Float64
     longNum[0] = value;
-
-    const r = Array.from(new Uint8Array(buffer)).reverse(); // reverse to get little endian
-    console.log(r.map((v) => v.toString(2)));
-    return r as any;
+    const bytes = Array.from(new Uint8Array(buffer));
+    return (bigEndianOrder ? bytes.reverse() : bytes) as TUint8;
 }
