7c9738829a484183b54f9fb365dc095818b2f700
Alexander Cheprasov <acheprasov84@gmail.com>
2023-09-26 21:46:45 +0100

    upd

--- src/JSBT.test.ts
+++ src/JSBT.test.ts
@@ -1,5 +1,7 @@
 import { JSBT } from './JSBT';
+import { delaySender } from './_tests/utils/delaySender';
 import { expectAsBinaryString } from './_tests/utils/expectAsBinaryString';
+import ByteStream from './reader/ByteStream';
 
 describe('JSBT', () => {
     describe('encode', () => {
@@ -20,42 +22,31 @@ describe('JSBT', () => {
             expectAsBinaryString(JSBT.encode(obj)).toEqual(
                 // root object, refID 0
                 '01110001 00000100 ' +
-                // userA key, refID 1
-                '00010001 00000101 01110101 01110011 01100101 01110010 01000001 ' +
-                // userA object, refID 2
-                '01110001 00000001 ' +
-                // name refID 3
-                '00010001 00000100 01101110 01100001 01101101 01100101 ' +
-                // Alex refID 4
-                '00010001 00000100 01000001 01101100 01100101 01111000 ' +
-                // userB key refID 5
-                '00010001 00000101 01110101 01110011 01100101 01110010 01000010 ' +
-                // userB object refID 6
-                '01110001 00000001 ' +
-                // ref name
-                '10110001 00000011 ' +
-                // ref Alex
-                '10110001 00000100 ' +
-                // userC key // refID 7
-                '00010001 00000101 01110101 01110011 01100101 01110010 01000011 ' +
-                // copy ref to userB object // refID 8
-                '10111001 00000110 ' +
-                // userD key // refID 9
-                '00010001 00000101 01110101 01110011 01100101 01110010 01000100 ' +
-                // link ref to userC object
-                '10110001 00001000'
+                    // userA key, refID 1
+                    '00010001 00000101 01110101 01110011 01100101 01110010 01000001 ' +
+                    // userA object, refID 2
+                    '01110001 00000001 ' +
+                    // name refID 3
+                    '00010001 00000100 01101110 01100001 01101101 01100101 ' +
+                    // Alex refID 4
+                    '00010001 00000100 01000001 01101100 01100101 01111000 ' +
+                    // userB key refID 5
+                    '00010001 00000101 01110101 01110011 01100101 01110010 01000010 ' +
+                    // userB object refID 6
+                    '01110001 00000001 ' +
+                    // ref name
+                    '10110001 00000011 ' +
+                    // ref Alex
+                    '10110001 00000100 ' +
+                    // userC key // refID 7
+                    '00010001 00000101 01110101 01110011 01100101 01110010 01000011 ' +
+                    // copy ref to userB object // refID 8
+                    '10111001 00000110 ' +
+                    // userD key // refID 9
+                    '00010001 00000101 01110101 01110011 01100101 01110010 01000100 ' +
+                    // link ref to userC object
+                    '10110001 00001000'
             );
-
-            console.time('JSBT');
-            const jsbt = JSBT.encode(obj);
-            console.timeEnd('JSBT');
-
-            console.time('JSON');
-            const json = JSON.stringify(obj);
-            console.timeEnd('JSON');
-
-            console.log('JSBT', 'len', jsbt.length, '\n', '\n');
-            console.log('JSON', 'len', json.length, '\n', '\n');
         });
     });
 
@@ -148,7 +139,7 @@ describe('JSBT', () => {
         it('should decode JSBT message with circular refs correct', () => {
             const foo: any = {
                 bar: {},
-                baz: {}
+                baz: {},
             };
             foo.bar.baz = foo.baz;
             foo.baz.bar = foo.bar;
@@ -166,6 +157,113 @@ describe('JSBT', () => {
             };
             const res = JSBT.decode(JSBT.encode(birthdays));
             expect(res).toEqual(birthdays);
+        });
+    });
+
+    describe('decodeStream', () => {
+        it('should decode stream message', async () => {
+            const obj = {
+                constants: {
+                    true: true,
+                    false: false,
+                    null: null,
+                    undefined: undefined,
+                    NaN: NaN,
+                    PosInfinity: Infinity,
+                    NegInfinity: -Infinity,
+                },
+                strings: {
+                    user: 'Alex',
+                    profession: 'IT',
+                    country: '🇬🇧',
+                },
+                integers: [0, -0, 1, -1, 256, -256, 12_345_678, -12_345_678],
+                floats: [10.1, 17.34, 3.1415, -3.1415, 0.335],
+                bigInts: [
+                    0n,
+                    10n,
+                    -13353n,
+                    132131231231235535525525325235235235235235n,
+                    -1343242345225252582365862385628653n,
+                ],
+                arrays: [
+                    [1, 2, 3],
+                    [4, 5, 6, 7, 8],
+                    [, , , , , 10],
+                ],
+                typedArrays: [
+                    new Uint16Array([1, 2, 3, 256, 1267, 134255]),
+                    new Int32Array([1, 2, -3, -256, 1267, -134255]),
+                    new Uint8Array([1, 2, 3, 255]),
+                ],
+                objects: {
+                    foo: {
+                        bar: {
+                            baz: 42,
+                            bar: 43,
+                            foo: 17,
+                        },
+                    },
+                },
+                sets: [new Set(['foo', 'baz', 'bar']), new Set(['1', '2', 1, 2])],
+                maps: [
+                    new Map([
+                        ['foo', 42],
+                        ['baz', 43],
+                        ['bar', 17],
+                    ]),
+                    new Map<any, any>([
+                        [{}, 'foo'],
+                        [[], 'bar'],
+                        [true, 'baz'],
+                    ]),
+                ],
+                symbols: {
+                    [Symbol.for('foo')]: Symbol.for('42'),
+                    [Symbol.for('bar')]: Symbol.for('12'),
+                    [Symbol.for('baz')]: Symbol.for('111'),
+                },
+                dates: {
+                    today: new Date('2023-09-26T21:37:00Z'),
+                    tomorrow: new Date('2023-09-27T21:37:00Z'),
+                    yesterday: new Date('2023-09-25T21:37:00Z'),
+                },
+            };
+
+            const stream = new ByteStream();
+            delaySender(stream, [JSBT.encode(obj)]);
+
+            const res = await JSBT.decodeStream(stream);
+            expect(res).toEqual(obj);
+        });
+
+        it('should allow to decode several joined messages', async () => {
+            const stream = new ByteStream();
+            delaySender(stream, [
+                JSBT.encode(42),
+                JSBT.encode('Alex'),
+                JSBT.encode('foo'),
+                JSBT.encode('bar'),
+                JSBT.encode('bar'),
+                JSBT.encode('bar'),
+                JSBT.encode('bar'),
+                JSBT.encode('foo'),
+                JSBT.encode('Alex'),
+                JSBT.encode('Alex'),
+                JSBT.encode(42)
+            ]);
+
+            expect(await JSBT.decodeStream(stream)).toBe(42);
+            expect(await JSBT.decodeStream(stream)).toBe('Alex');
+            expect(await JSBT.decodeStream(stream)).toBe('foo');
+            expect(await JSBT.decodeStream(stream)).toBe('bar');
+            expect(await JSBT.decodeStream(stream)).toBe('bar');
+            expect(await JSBT.decodeStream(stream)).toBe('bar');
+            expect(await JSBT.decodeStream(stream)).toBe('bar');
+            expect(await JSBT.decodeStream(stream)).toBe('foo');
+            expect(await JSBT.decodeStream(stream)).toBe('Alex');
+            expect(await JSBT.decodeStream(stream)).toBe('Alex');
+            expect(await JSBT.decodeStream(stream)).toBe(42);
         });
     });
 });
